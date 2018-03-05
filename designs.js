5ef86f2ab6f464133157222a0bbc38330b5a51c4
= <=>
2018-03-06 01:08:08 +0200

    retrieved the width and height values from the input fields

--- designs.js
+++ designs.js
@@ -1,10 +1,26 @@
 // Select color input
 // Select size input
+let heightInput = document.querySelector('#inputHeight');
+
+heightInput.addEventListener('change',function(event){
+    height = event.target.value;
+});
+
+let inputWeight = document.querySelector('#inputWeight');
+
+inputWeight.addEventListener('change', function(event){
+    width = event.target.value;
+    console.log(width);
+});
+
+
+
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
-// Your code goes here!
+    height = document.getElementById('#inputHeight').value;
+    console.log(height);
 
 }
