79bbe8324df59066edeaac7000d99419bf87286d
= <=>
2018-03-06 21:53:51 +0200

    retrieved the color value

--- designs.js
+++ designs.js
@@ -1,9 +1,17 @@
 // Select color input
+let colorInput = document.querySelector('#colorPicker');
+
+colorInput.addEventListener('change', function(event){
+    color = event.target.value;
+    console.log(color);
+});
+
 // Select size input
 let heightInput = document.querySelector('#inputHeight');
 
 heightInput.addEventListener('change',function(event){
     height = event.target.value;
+    console.log(height);
 });
 
 let inputWeight = document.querySelector('#inputWeight');
@@ -12,9 +20,6 @@ inputWeight.addEventListener('change', function(event){
     width = event.target.value;
     console.log(width);
 });
-
-
-
 
 // When size is submitted by the user, call makeGrid()
 
